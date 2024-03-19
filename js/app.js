2e13bb4165aebd180a1647a4ccd389566ff1a0c7
joao-braganca <jvdbraganca@gmail.com>
2024-03-19 12:28:02 -0300

    adicionando mudanca ao clicar no botao

---
+++ js/app.js
@@ -0,0 +1,5 @@
+function criptografar(){
+    document.getElementById("div_resultado_copiar").style.setProperty("visibility", "visible");
+    document.getElementById("div_resultado").style.setProperty("visibility", "hidden");
+
+}
